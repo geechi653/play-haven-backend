a779659fd37d2d901cc773d56895105d61ba2143
AlexSvet117 <alex.svet117@gmail.com>
2025-06-06 19:27:35 +0100

    Profile repo refactor

--- app/repositories/profile_repository.py
+++ app/repositories/profile_repository.py
@@ -1,7 +1,9 @@
 from app.models.profile import Profile
 from app.extensions import db
+from typing import Optional
 
 class ProfileRepository:
+
     @staticmethod
     def create_profile(user_id: int, first_name: str, last_name: str, country: str) -> Profile:
         profile = Profile(user_id=user_id, first_name=first_name, last_name=last_name, country=country)
@@ -9,6 +11,16 @@ class ProfileRepository:
         db.session.flush()
         return profile
     
+
+    @staticmethod
+    def get_profile_by_user_id(user_id: int) -> Optional[Profile]:
+        return db.session.query(Profile).filter_by(user_id=user_id).first()
+    
+    @staticmethod
+    def get_profile_by_profile_id(profile_id: int) -> Optional[Profile]:
+        return db.session.query(Profile).filter_by(profile_id=profile_id).first()
+        
+
     @staticmethod
     def update_profile_by_user_id(user_id: int, **kwargs) -> bool:
         profile = db.session.query(Profile).filter_by(user_id=user_id).first()
@@ -21,6 +33,20 @@ class ProfileRepository:
         db.session.flush()
         return profile
     
+
+    @staticmethod
+    def update_profile_by_profile_id(profile_id: int, **kwargs) -> Profile:
+        profile = db.session.get(Profile, profile_id)
+        if not profile:
+            raise ValueError(f"Profile with id {profile_id} not found")
+        
+        for key, value in kwargs.items():
+            if hasattr(profile, key):
+                setattr(profile, key, value)
+        db.session.flush()
+        return profile
+    
+
     @staticmethod
     def delete_profile_by_user_id(user_id: int) -> Profile:
         profile = db.session.query(Profile).filter_by(user_id=user_id).first()
@@ -31,6 +57,7 @@ class ProfileRepository:
         db.session.flush()
         return True
     
+
     @staticmethod
     def delete_profile_by_profile_id(profile_id: int) -> Profile:
         profile = db.session.get(Profile, profile_id)
