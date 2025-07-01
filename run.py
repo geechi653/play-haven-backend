2b91210078b7620463791913a817953f21c8c3ef
geechi653 <ragilarrow@gmail.com>
2025-06-30 22:57:51 -0400

    fixed run.py for deploy

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 if __name__ == "__main__":
     env = os.getenv("FLASK_ENV", "development")
     app = create_app(env)
-    app.run(debug=(env == "development"), port=5001)
+    app.run(host="0.0.0.0", port=5000)
